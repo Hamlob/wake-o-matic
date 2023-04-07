ad6d9186d23f58e4783d15a893e25d25982bd3b3
Charles Cameron <Jenibluere@gmail.com>
2023-04-07 19:04:14 +0100

    Outline unit tests for action functionality.
    Co-authored-by: MrMeerkat <26203158+MrMeerkat@users.noreply.github.com>

--- test/src/cppTests.cpp
+++ test/src/cppTests.cpp
@@ -2,6 +2,13 @@
 
 int main() {
     std::cout << "Tests started!" << std::endl;
+
+    test_listener_triggered_by_sleeping();
+    test_listener_triggered_by_warning();
+    // test_speaker_plays_warning();
+    // test_speaker_plays_alarm();
+    // test_no_action_if_no_flags();
+
     std::cout << "Tests (0) succeeded!" << std::endl;
     return 0; // You can put a 1 here to see later that it would generate an error
 }
