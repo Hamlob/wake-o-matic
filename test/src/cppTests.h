0a485e221043da7fb177d6a9fb38ad3a7cd50308
Charlie Cameron <jenibluere@gmail.com>
2023-04-10 19:56:10 +0100

    Add comments to test functions

--- test/src/cppTests.h
+++ test/src/cppTests.h
@@ -4,7 +4,18 @@
 
 #define assertm(exp, msg) assert(((void)msg, exp))
 
+/// @brief Creates action thread and ensures sleeping flag is set correctly
+/// @return True if test completed
 bool test_sleeping_flag_set();
+
+/// @brief Creates action thread and ensures warning flag is set correctly
+/// @return True if test completed
 bool test_warning_flag_set();
+
+/// @brief Creates action thread and checks thread executes action logic when a flag is set
+/// @return True if test completed
 bool test_listener_triggered_by_sleeping();
+
+/// @brief Creates action thread and checks thread executes action logic when a flag is set
+/// @return True if test completed
 bool test_listener_triggered_by_warning();
